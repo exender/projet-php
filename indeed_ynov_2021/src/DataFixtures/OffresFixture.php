41fbb1e105f3fe82700395faa5501f0481126bbb
Biozard01 <arthur.laforest33@gmail.com>
2020-11-16 17:46:45 +0100

    site end

--- indeed_ynov_2021/src/DataFixtures/OffresFixture.php
+++ indeed_ynov_2021/src/DataFixtures/OffresFixture.php
@@ -38,29 +38,36 @@ class OffresFixture extends Fixture
         $manager->persist($temps_partiel);
 
         //offres
-        for ($i=0; $i < 25; $i++) { 
-           $contrat = $faker->randomElement($Array = array($cdd , $freelance, $cdi));
-           $debut_contrat = $faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now');
-           if ($contrat != $cdi) {
-               $fin_de_mission = $faker->dateTimeBetween($startDate ='now',$endDate ='5 years');
-           }
-           else {
-               $fin_de_mission = null;
-           }
-           $offre = new Offres();
-           $offre->setTitre($faker->sentence($nbWords = 10, $variableNbWords = true))
-                 ->setDescription($faker->paragraph($nbSentences = 5, $variableNbSentences = true))
-                 ->setAdresse($faker->address)
-                 ->setCodepostal(str_replace(' ', '', $faker->postcode))
-                 ->setVille($faker->city)
-                 ->setCreatedat($debut_contrat)
-                 ->setUpdatedat($faker->dateTimeBetween($startDate = $debut_contrat, $endDate = 'now', $timezone = null))
-                 ->setMissionend($fin_de_mission)
-                 ->setContrat($contrat)
-                 ->setTypecontrat($faker->randomElement($array = array($temps_plein, $temps_partiel)));
-           $manager->persist($offre);     
-        }    
+        for ($i = 0; $i < 25; $i++) {
+            $titre = $faker->company;
+            $desc = $faker->paragraph($nbSentences = 5, $variableNbSentences = true);
+            $adresse = $faker->address;
+            $code_post = str_replace(' ', '', $faker->postcode);
+            $ville = $faker->city;
+            $debut_contrat = $faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now');
+            $up_date = $faker->dateTimeBetween($startDate = $debut_contrat, $endDate = 'now', $timezone = null);
+            $contrat = $faker->randomElement($Array = array($cdd, $freelance, $cdi));
+            $type = $faker->randomElement($array = array($temps_plein, $temps_partiel));
+
+            if ($contrat != $cdi) {
+                $fin_de_mission = $faker->dateTimeBetween($startDate = 'now', $endDate = '5 years');
+            } else {
+                $fin_de_mission = null;
+            }
+
+            $offre = new Offres();
+            $offre->setTitre($titre)
+                ->setDescription($desc)
+                ->setAdresse($adresse)
+                ->setCodepostal($code_post)
+                ->setVille($ville)
+                ->setCreatedat($debut_contrat)
+                ->setUpdatedat($up_date)
+                ->setMissionend($fin_de_mission)
+                ->setContrat($contrat)
+                ->setTypecontrat($type);
+            $manager->persist($offre);
+        }
         $manager->flush();
     }
 }
-/////////fafefazef
