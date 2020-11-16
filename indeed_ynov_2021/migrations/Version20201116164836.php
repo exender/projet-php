c1e73642d149749aee05b8516725699742587fac
Biozard01 <arthur.laforest33@gmail.com>
2020-11-16 17:58:00 +0100

    finished ! :)

--- indeed_ynov_2021/migrations/Version20201116164836.php
+++ indeed_ynov_2021/migrations/Version20201116164836.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201116133553 extends AbstractMigration
+final class Version20201116164836 extends AbstractMigration
 {
     public function getDescription() : string
     {
